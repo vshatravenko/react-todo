14573cbfe15de5c117501403b1af5774fe31d981
vshatravenko <vshatravenko@heliostech.fr>
2018-11-25 14:51:51 +0200

    Add the base ToDoList component

---
+++ src/components/ToDoList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+
+class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid container spacing={24}>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default ToDoList;
